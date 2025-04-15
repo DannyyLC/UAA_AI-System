75b9fc252922218a6c350cb0494a6528d48a803f
Danny Limon <cervantesdaniellimon50m@gmail.com>
2025-04-15 12:04:10 -0600

    Estructura del testing

---
+++ Data/questions.py
@@ -0,0 +1,31 @@
+""" Listas con las preguntas con las cuales se evaluara el desempeño de los modelos 
+            Se estaran usando aproximadamente 10 preguntas de cada area"""
+# Tecnologia
+tecnologia = []
+
+# IA
+ia = []
+
+# Matematicas
+matematicas = []
+
+# Quimica
+quimica = []
+
+# Leyes
+leyes = []
+
+# Finanzas
+finanzas = []
+
+# Biologia
+biologia = []
+
+# Automotriz
+automotriz = []
+
+# Medicina 
+medicina = [] 
+  
+# Fisica
+fisica = []
